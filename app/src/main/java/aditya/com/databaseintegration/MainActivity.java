afaca4ecba0e0b1cf67e8b703afe2b844adfe3eb
Aditya Agarwal <skymanaditya1@gmail.com>
2015-07-31 16:29:23 +0530

    Initial Commit. Demonstrates the insertion of data into database in android

---
+++ app/src/main/java/aditya/com/databaseintegration/MainActivity.java
@@ -0,0 +1,130 @@
+package aditya.com.databaseintegration;
+
+import android.os.StrictMode;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Code to demonstrate the insertion of data into android application.
+ * Also demonstrates how to retrieve the data.
+ * Uses the localhost for local server.
+ * Php scripting language for backend integration.
+ */
+
+public class MainActivity extends ActionBarActivity {
+
+    EditText etName, etEmail, etPhone;
+    Button bSubmit;
+    public static String url = "http://splitmoney.site50.net/workshop_connect.php";
+    String name, email, phone;
+    InputStream is = null;
+    String exceptionMessage = "There seems to be some problem connecting to database. " +
+            "Please check your Internet Connection and try again.";
+    String successMessage = "Data has been entered successfully";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPhone = (EditText) findViewById(R.id.etPhone);
+        bSubmit = (Button) findViewById(R.id.button_submit);
+
+        bSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                name = etName.getText().toString();
+                email = etEmail.getText().toString();
+                phone = etPhone.getText().toString();
+
+                if(name.equals("") ||
+                        email.equals("") ||
+                        phone.equals("")){
+                    String msg = "One or more fields are empty";
+                    etName.setText("");
+                    etEmail.setText("");
+                    etPhone.setText("");
+                }
+                else{
+
+                    List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
+                    nameValuePairList.add(new BasicNameValuePair("name", name));
+                    nameValuePairList.add(new BasicNameValuePair("email", email));
+                    nameValuePairList.add(new BasicNameValuePair("phone", phone));
+
+                    try{
+                        HttpClient httpClient = new DefaultHttpClient();
+                        HttpPost httpPost = new HttpPost(url);
+                        httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairList));
+                        HttpResponse httpResponse = httpClient.execute(httpPost);
+                        HttpEntity httpEntity = httpResponse.getEntity();
+                        is = httpEntity.getContent();
+                        etName.setText("");
+                        etEmail.setText("");
+                        etPhone.setText("");
+                        Toast.makeText(getApplicationContext(), successMessage, Toast.LENGTH_SHORT).show();
+                    }catch(IOException e){
+                        Toast.makeText(getApplicationContext(), exceptionMessage, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
